62f5eb53a0b83ed8d177067ce8b3cd24b431046b
peto123455 <peto123455@gmail.com>
2023-05-31 13:45:21 +0200

    Removed unused usings

--- semestralka/Constants.cs
+++ semestralka/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace semestralka.utils
+﻿namespace semestralka.utils
 {
     public static class Constants {
         public const string ERROR = "CHYBA !";
--- semestralka/FileHandlerWriter.cs
+++ semestralka/FileHandlerWriter.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace semestralka
 {
